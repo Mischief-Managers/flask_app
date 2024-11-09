3c46323353fcec710a522651cf32c78e292b88bb
lhamu <mudita67@gmail.com>
2024-11-09 12:47:09 +0200

    API for saving records from file

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import json
+import uuid
 import requests
+import pandas as pd
 from pymongo import MongoClient
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 mongo_client = MongoClient('localhost', 27017)
  
-@app.route('/add-record')
+@app.route('/add-record', methods=["POST"])
 def add_record():  
     response = requests.json()
+    record_id = uuid.uuid4()
+    response["record_id"] = record_id
     mongo_client = mongo_client.flask_db
     db = mongo_client.records
     insertion = db.insert_one(response)
 
     return json.dumps({'message': 'success'}), 200, {'Content-Type': 'application/json'}
 
-@app.route('/get-records')
+@app.route('/get-records', methods=["GET"])
 def get_records():
     mongo_client = mongo_client.flask_db
     db = mongo_client.records
@@ -23,7 +27,7 @@ def get_records():
     return json.dumps(records), 200, {'Content-Type': 'application/json'}
  
 
-@app.route('/get-specific-record')
+@app.route('/get-specific-record', methods=["POST"])
 def get_specific_record():
     record_id = request.json.get("record_id")  
     mongo_client = mongo_client.flask_db
@@ -31,6 +35,14 @@ def get_specific_record():
     record = db.find_one({"record_id": record_id})
     return json.dumps(record), 200, {'Content-Type': 'application/json'}
 
+@app.route('/add-records-from-file', methods=["POST"])
+def add_records_from_file():
+    file_path = request.json.get("file_path")
+    mongo_client = mongo_client.flask_db
+    db = mongo_client.records
+    records = pd.read_csv(file_path).to_dict('records')
+    insertion = db.insert_many(records)
+    return json.dumps({'message': 'success'}), 200, {'Content-Type': 'application/json'}
 
 if __name__ == '__main__':
    app.run(debug=False, host='0.0.0.0', port=4500)
